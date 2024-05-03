656387d0d4359b19e76f941050bad7916b235b6e
bharathj14 <bharath.141293@gmail.com>
2024-05-03 11:27:31 +0530

    first commit - creating 3 service doctor patient and eureka server

---
+++ EurekaClientDoctor/EurekaClientDoctor/src/test/java/com/cloud/EurekaClientDoctor/EurekaClientDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.EurekaClientDoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientDoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ EurekaClientPaitent/EurekaClientPaitent/src/test/java/com/cloud/EurekaClientPaitent/EurekaClientPaitentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.EurekaClientPaitent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientPaitentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
